111341ca436de47686380dbb6028ea4e68bae07d
Sung Kim <sungmkim80@gmail.com>
2018-04-01 09:40:19 -0400

    first commit

---
+++ src/pages/page-2.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "gatsby-link";
+import Container from "./container";
+
+export default () => (
+  <Container>
+    <h2>Page 2!</h2>
+    <br />
+    <Link to="/">Home</Link>
+  </Container>
+);
---
+++ src/utils/typography.js
@@ -0,0 +1,10 @@
+import Typography from "typography";
+// import bootstrapTheme from "typography-theme-bootstrap";
+// const typography = new Typography(bootstrapTheme);
+// import lawtonTheme from "typography-theme-lawton";
+// const typography = new Typography(lawtonTheme);
+
+import githubTheme from "typography-theme-github";
+const typography = new Typography(githubTheme);
+
+export default typography;
