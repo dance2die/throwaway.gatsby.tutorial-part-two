786b89ffd1bfd8d87c95399783e042cf09032b46
Sung Kim <sungmkim80@gmail.com>
2018-04-01 10:08:30 -0400

    fixed import statements

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "gatsby-link";
-import Container from "../components/Container";
+import Container from "../components/container";
 
 export default () => (
   <Container>
